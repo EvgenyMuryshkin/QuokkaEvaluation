ae0b848b61360685c5bd4e6d3ba16396e6c9d16c
Evgeny Muryshkin <evmuryshkin@gmail.com>
2018-08-26 14:37:49 +1000

    split ROT2U code into separate project

--- ROT2U/ROT2UDevice/ROT2UController.cs
+++ ROT2U/ROT2UDevice/ROT2UController.cs
@@ -8,21 +8,11 @@ using System.Threading.Tasks;
 namespace SnakeGame
 {
     [BoardConfig(Name = "Quokka")]
-    public static class LEDController
+    public static class ROT2UController
 	{
         public static async Task Aggregator(
             // blinker
             FPGA.OutputSignal<bool> LED1,
-
-            // keypad
-            FPGA.OutputSignal<bool> K7,
-            FPGA.OutputSignal<bool> K6,
-            FPGA.OutputSignal<bool> K5,
-            FPGA.OutputSignal<bool> K4,
-            FPGA.InputSignal<bool> K3,
-            FPGA.InputSignal<bool> K2,
-            FPGA.InputSignal<bool> K1,
-            FPGA.InputSignal<bool> K0,
 
             // banks 
             FPGA.OutputSignal<bool> Bank1,
@@ -31,41 +21,25 @@ namespace SnakeGame
 
 			// WS2812
 			FPGA.OutputSignal<bool> DOUT,
-
-            // ADC
-            FPGA.OutputSignal<bool> ADC1NCS,
-            FPGA.OutputSignal<bool> ADC1SLCK,
-            FPGA.OutputSignal<bool> ADC1DIN,
-            FPGA.InputSignal<bool> ADC1DOUT,
-
-            // SERVO
-            FPGA.OutputSignal<bool> Servo,
 			
 			// UART
 			FPGA.InputSignal<bool> RXD,
 			FPGA.OutputSignal<bool> TXD
+
+			// SERVO IO is generated inside handlers
 			)
         {
             QuokkaBoard.OutputBank(Bank1);
             QuokkaBoard.InputBank(Bank2);
 			QuokkaBoard.OutputBank(Bank3);
 
-            GameControlsState controlsState = new GameControlsState();
-
             IsAlive.Blink(LED1);
 
-            Peripherals.GameControls(
-                ADC1NCS, ADC1SLCK, ADC1DIN, ADC1DOUT,
-                K7, K6, K5, K4, K3, K2, K1, K0,
-                ref controlsState);
-
-            LEDControl(controlsState.keyCode, DOUT, Servo, RXD, TXD);
+            DeviceControl(DOUT, RXD, TXD);
         }
 
-        public static void LEDControl(
-            KeypadKeyCode keyCode, 
+        public static void DeviceControl(
             FPGA.OutputSignal<bool> DOUT,
-            FPGA.OutputSignal<bool> Servo,
 			FPGA.InputSignal<bool> RXD,
 			FPGA.OutputSignal<bool> TXD
             )
@@ -76,18 +50,6 @@ namespace SnakeGame
 
             bool internalDOUT = false;
             FPGA.Config.Link(internalDOUT, DOUT);
-
-            byte servoValue = 0;
-			bool internalServo = false;
-			FPGA.Config.Link(internalServo, Servo);
-			//MG996R.Continuous(servoValue, Servo);
-
-			Action testHandler = () =>
-			{
-				servoValue = servoValue == 0 ? (byte)180 : (byte)0;
-				MG996R.Write(servoValue, out internalServo);
-			};
-			FPGA.Config.OnTimer(TimeSpan.FromSeconds(1), testHandler);
 
 			const uint baud = 115200;
 
@@ -149,7 +111,7 @@ namespace SnakeGame
 					}
 				}
 
-				Graphics.Draw(buff, out internalDOUT);
+				WS2812B.SyncWrite(buff, 0, buff.Length, out internalDOUT);
 			};
 			FPGA.Config.OnStream(heartBeat, ledHandler);
 
