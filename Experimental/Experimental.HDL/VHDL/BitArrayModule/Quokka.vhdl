0404210dc8bae677a0c8f0c404b8e1ffc7279b44
Evgeny Muryshkin <evmuryshkin@gmail.com>
2020-05-02 17:36:49 +1000

    update generated lib

--- Experimental/Experimental.HDL/VHDL/BitArrayModule/Quokka.vhdl
+++ Experimental/Experimental.HDL/VHDL/BitArrayModule/Quokka.vhdl
@@ -22,6 +22,8 @@ package Quokka is
 		signal clk: in std_logic;
 		signal reset: in std_logic;
 		signal internalReset: in std_logic);
+
+	function bit_to_integer( s : std_logic ) return integer;
 end package Quokka;
 
 -- package implementation
@@ -53,6 +55,16 @@ package body Quokka is
 		
 		bs.Starting <= bs.Running AND NOT bs.Started;
 	end procedure;
+
+	function bit_to_integer( s : std_logic ) return integer is
+		begin
+			  if s = '1' then
+			  return 1;
+		   else
+			  return 0;
+		   end if;
+		end function;
+
 end package body Quokka;
 
 library ieee;
