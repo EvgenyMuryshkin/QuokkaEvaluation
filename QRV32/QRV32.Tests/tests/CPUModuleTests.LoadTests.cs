9bbd7a06a314b7b54fd48da0c6419afc69bae8b1
Evgeny Muryshkin <evmuryshkin@gmail.com>
2020-05-31 22:08:19 +1000

    LBU test

--- QRV32/QRV32.Tests/tests/CPUModuleTests.LoadTests.cs
+++ QRV32/QRV32.Tests/tests/CPUModuleTests.LoadTests.cs
@@ -21,5 +21,20 @@ namespace QRV32.CPUModuleTests
             Assert.AreEqual(0x3U, tl.Regs.State.x[4]);
             Assert.AreEqual(0xFFFFFFF4U, tl.Regs.State.x[5]);
         }
+
+        [TestMethod]
+        public void LBU()
+        {
+            var sim = PowerUp();
+            sim.MemoryBlock[0x100] = 0xF4030201;
+
+            var tl = sim.TopLevel;
+            var instructions = Inst.FromAsmFile("lbu");
+            sim.RunAll(instructions);
+            Assert.AreEqual(0x1U, tl.Regs.State.x[2]);
+            Assert.AreEqual(0x2U, tl.Regs.State.x[3]);
+            Assert.AreEqual(0x3U, tl.Regs.State.x[4]);
+            Assert.AreEqual(0xF4U, tl.Regs.State.x[5]);
+        }
     }
 }
