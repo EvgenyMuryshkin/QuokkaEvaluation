774f6bbfd21489658ab5d369fee9331921540015
Evgeny Muryshkin <evmuryshkin@gmail.com>
2020-06-01 17:13:19 +1000

    load instructions tests

--- QRV32/QRV32.Tests/tests/CPUModuleTests.LoadTests.cs
+++ QRV32/QRV32.Tests/tests/CPUModuleTests.LoadTests.cs
@@ -36,5 +36,53 @@ namespace QRV32.CPUModuleTests
             Assert.AreEqual(0x3U, tl.Regs.State.x[4]);
             Assert.AreEqual(0xF4U, tl.Regs.State.x[5]);
         }
+
+        [TestMethod]
+        public void LH()
+        {
+            var sim = PowerUp();
+            sim.MemoryBlock[0x0FC] = 0xE4030201U;
+            sim.MemoryBlock[0x100] = 0xF4030201U;
+            sim.MemoryBlock[0x104] = 0x07030201U;
+
+            var tl = sim.TopLevel;
+            var instructions = Inst.FromAsmFile("lh");
+            sim.RunAll(instructions);
+            Assert.AreEqual(0x0201U, tl.Regs.State.x[2]);
+            Assert.AreEqual(0xFFFFF403U, tl.Regs.State.x[3]);
+            Assert.AreEqual(0xFFFFE403U, tl.Regs.State.x[4]);
+        }
+
+        [TestMethod]
+        public void LHU()
+        {
+            var sim = PowerUp();
+            sim.MemoryBlock[0x0FC] = 0xE4030201U;
+            sim.MemoryBlock[0x100] = 0xF4030201U;
+            sim.MemoryBlock[0x104] = 0x07030201U;
+
+            var tl = sim.TopLevel;
+            var instructions = Inst.FromAsmFile("lhu");
+            sim.RunAll(instructions);
+            Assert.AreEqual(0x0201U, tl.Regs.State.x[2]);
+            Assert.AreEqual(0xF403U, tl.Regs.State.x[3]);
+            Assert.AreEqual(0xE403U, tl.Regs.State.x[4]);
+        }
+
+        [TestMethod]
+        public void LW()
+        {
+            var sim = PowerUp();
+            sim.MemoryBlock[0x0FC] = 0xE4030201U;
+            sim.MemoryBlock[0x100] = 0xF4030201U;
+            sim.MemoryBlock[0x104] = 0x07030201U;
+
+            var tl = sim.TopLevel;
+            var instructions = Inst.FromAsmFile("lw");
+            sim.RunAll(instructions);
+            Assert.AreEqual(0xE4030201U, tl.Regs.State.x[2]);
+            Assert.AreEqual(0xF4030201U, tl.Regs.State.x[3]);
+            Assert.AreEqual(0x07030201U, tl.Regs.State.x[4]);
+        }
     }
 }
